0275d394f7902083632a9d6f678a01a9e965ea51
Allison Nguyen <90017503+allisonnguyen@users.noreply.github.com>
2026-03-09 12:52:33 -0400

    Update circle.js

--- circle.js
+++ circle.js
@@ -1,14 +1,39 @@
 class Circle {
-	constructor(x, y, d) {
-		this.x = x;
-		this.y = y;
-		this.d = d;
+	constructor(x, y, rMax, growthSpeed) {
+	    this.x = x;
+	    this.y = y;
+
+	    this.r = 0;              // start at 0
+	    this.rMax = rMax;        // max size
+	    this.growthSpeed = growthSpeed;
+
+	    this.direction = 1;      // 1 = growing, -1 = shrinking
+	    this.finished = false;
+	}
+
+	update() {
+	    if (this.finished) return;
+
+	    this.r += this.growthSpeed * this.direction;
+
+	    // switch to shrinking
+	    if (this.direction === 1 && this.r >= this.rMax) {
+	      this.direction = -1;
+	    }
+
+	    // stop when fully shrunk
+	    if (this.direction === -1 && this.r <= 0) {
+	      this.finished = true;
+	      this.r = 0;
+	    }
 	}
 
 	display() {
+		if (this.finished) return;
+
 		noFill();
 		stroke(255, 255, 255);
 		strokeWeight(1.5);
-		ellipse(this.x, this.y, this.d, this.d);
+		ellipse(this.x, this.y, this.r * 2, this.r * 2);
 	}
 }
